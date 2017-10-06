cc5d9f2602da3853095f269b702717c61edf8032
Alexandre Magro <alexandremagro@live.com>
2017-10-06 02:51:19 -0300

    Fix: Replacing Brunch with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  entry: './src/modele.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    library: 'modele',
+    libraryTarget: 'umd',
+    filename: 'modele.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+    new UglifyJSPlugin()
+  ]
+}
