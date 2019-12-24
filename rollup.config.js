0a4b37534c6466f89d24e4673e773a5f1f87a793
Alexandre Magro <alexandremagro@live.com>
2019-12-24 19:12:39 -0300

    Remove UMD bundles, minifies
    - Remove UMD bundles at this moment;
    - No need minification for CJS and ESM modules now;
    - Disable "modules" at babel on rollup;

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,33 @@
 import resolve from '@rollup/plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
-import { terser } from 'rollup-plugin-terser'
-import { uglify } from 'rollup-plugin-uglify'
+import babel from 'rollup-plugin-babel'
+import pkg from './package.json'
 
 const base = {
-  input: 'src',
   plugins: [
     resolve(),
     commonjs(),
-    babel()
+    babel({
+      babelrc: false,
+      presets: [["@babel/env", { "modules": false }]],
+      exclude: ["node_modules/**/*"]
+    })
   ]
 }
 
 const main = Object.assign({}, base, {
   input: 'src/core/model',
   output: [
-    { file: 'dist/modele.cjs.js', format: 'cjs' },
-    { file: 'dist/modele.esm.js', format: 'esm' },
-    {
-      file: 'dist/modele.js',
-      format: 'umd',
-      name: 'Modele'
-    },
-    {
-      file: 'dist/modele.min.js',
-      format: 'umd',
-      name: 'Modele',
-      plugins: [terser(), uglify()]
-    }
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'esm' }
   ]
 })
 
 const validations = Object.assign({}, base, {
   input: 'src/validations',
   output: [
-    { file: 'dist/validations/index.cjs.js', format: 'cjs' },
-    { file: 'dist/validations/index.esm.js', format: 'esm' },
-    {
-      file: 'dist/validations/index.js',
-      format: 'umd',
-      name: 'ModelValidations'
-    },
-    {
-      file: 'dist/validations/index.min.js',
-      format: 'umd',
-      name: 'ModelValidations',
-      plugins: [terser(), uglify()]
-    }
+    { file: 'dist/validations/index.js', format: 'cjs' },
+    { file: 'dist/validations/index.esm.js', format: 'esm' }
   ]
 })
 
